b91350b44bb65d6f268545e1a25b5026b7993be5
Moin <115621594+Moin5051@users.noreply.github.com>
2025-08-23 17:44:15 +0530

    Add files via upload
    Day 6 where the code is about what functions are and a code file for the challenge called maze in Reeborg.ca ---> Reborg's World

---
+++ Day6/Maze.py
@@ -0,0 +1,20 @@
+#This is a code for challendge in Reeborg's World Challenge maze. 
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
---
+++ Day6/functions.py
@@ -0,0 +1,22 @@
+# A function is differentiated when it has a () at the end of it.
+# to define your own new function we use the key word def
+
+def my_function():
+    #do this
+    #and this
+    #and finally this
+# any code inside the function will be indented. And to use the function or to "Call the function"
+# we use the name of the function
+    print("This is my first function in Python\n")
+
+
+my_function()
+
+
+#While Loop
+#similar to a for loop however, here until the condition is true the loop will keep running.
+
+abc=10
+while abc > 0:
+    print("Hi ")
+    abc-=1
